4f87c0e663f99b46d144cd6761f2b54a6648199b
Joey <joey@lineageos.org>
2019-01-13 10:21:11 +0100

    sm7250-common: Build Trust HAL
    Change-Id: I92706987f8d3094cdfb82e71e5331b7efe69e5df

--- common.mk
+++ common.mk
@@ -477,6 +477,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.1-service-qti
