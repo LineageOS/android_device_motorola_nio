4f02303c8bcfb3e2f94dd827635e8e6ea857463d
Santhosh Behara <santhoshbehara@codeaurora.org>
2019-12-19 17:22:17 +0530

    sm7250-common: qssi/base.mk: Remove loading codec2 s/w components
    Remove loading codec2 s/w components
    
    Change-Id: Id762b524f1ef46f76c2895f4f74f335b27572522

--- common.mk
+++ common.mk
@@ -347,11 +347,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2.xml
 
-# Media - Codec2 modules
-PRODUCT_PACKAGES += \
-    com.android.media.swcodec \
-    libsfplugin_ccodec
-
 # Misc
 PRODUCT_PACKAGES += \
     libchrome \
