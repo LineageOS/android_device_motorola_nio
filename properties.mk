c363f186f2f00b5895e5e3758fb73fed6773892b
Eric Biggers <ebiggers@google.com>
2020-01-14 15:48:44 -0800

    sm7250-common: Use the new inline crypto format and implementation
    Change the fileencryption option in redbull's fstab to specify the new
    inline-crypt-optimized format for encrypted files, rather than the old
    custom vendor-specific format.  The new format is supported by GKI and
    the upstream Linux kernel.
    
    In addition, add the 'inlinecrypt' mount option to enable inline
    encryption.  This is required to use inline encryption with the new
    implementation, since whether to use inline encryption is now a runtime
    setting rather than being implied by the encryption format.
    
    Bug: 147259927
    Change-Id: If85d2b39340965b394bc2aeced2b854d09ae0e17

--- properties.mk
+++ properties.mk
@@ -116,7 +116,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # Crypto
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.crypto.allow_encrypt_override=true \
-    ro.crypto.volume.filenames_mode=aes-256-cts
+    ro.crypto.dm_default_key.options_format.version=2 \
+    ro.crypto.volume.filenames_mode=aes-256-cts \
+    ro.crypto.volume.metadata.method=dm-default-key \
+    ro.crypto.volume.options=::v2
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
