3d08661f6e54d9225b100addb59fbe44479ab784
Roshan Pius <rpius@google.com>
2020-04-24 14:33:28 -0700

    sm7250-common: Add WifiOverlay
    Bug: 148617260
    Test: Device boots up and connects to wifi networks.
    Change-Id: Iec27a6d947a232a42451ef095dc73d1a52327db5

--- common.mk
+++ common.mk
@@ -513,6 +513,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.wifi.hostapd@1.1.vendor \
     vendor.qti.hardware.wifi.supplicant@2.0.vendor \
     vendor.qti.hardware.wifi.supplicant@2.1.vendor \
+    WifiOverlay \
     wpa_supplicant \
     wpa_supplicant.conf
 
