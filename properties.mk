0343ff69488214f2ac86e77a3158bfde85b8e328
David Sehr <sehr@google.com>
2020-04-25 15:50:40 -0700

    sm7250-common: DO NOT MERGE: Use dex2oat64 on sunfish
    Enable dex2oat64 use on sunfish.
    
    Bug: 153380900
    Test: boot and install an application
    Change-Id: I3e7a6e6e9385ff6564d1a2e6dda004ebb061f095
    (cherry picked from commit 126f03be80f57a8a0411842011152d9381589b78)
    Merged-In: I3e7a6e6e9385ff6564d1a2e6dda004ebb061f095

--- properties.mk
+++ properties.mk
@@ -119,6 +119,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat64.enabled=true \
     dalvik.vm.heapmaxfree=8m \
     dalvik.vm.heapminfree=512k \
     dalvik.vm.heapsize=512m \
