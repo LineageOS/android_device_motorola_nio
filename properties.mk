47d3d02682ee7b23c753e9942e6697ca1fd832bb
Anver sadhique <akurun@codeaurora.org>
2020-07-10 14:06:16 +0530

    sm7250-common: conf_files: set higher priority to c2 than OMX
    Change-Id: I338797dfa878ac43b1c93f7b7ebbee9783ea7034

--- properties.mk
+++ properties.mk
@@ -186,6 +186,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     media.settings.xml=/vendor/etc/media_profiles_vendor.xml \
     debug.stagefright.ccodec=1 \
+    debug.stagefright.omx_default_rank=0 \
     persist.vendor.media.recorder.bt709=true
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
