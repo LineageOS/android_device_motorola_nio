ef90ed9888b574b86e8b4325ab41250e8b3df3c3
Prasanta Kumar Sahu <prassa@codeaurora.org>
2020-07-22 20:59:02 +0530

    sm7250-common: Enable incremental FS feature.
    Change-Id: I12c2a493c07c738ef962798aa8dcac5eda071f54

--- properties.mk
+++ properties.mk
@@ -181,6 +181,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.vt_avail_ovr=1  \
     persist.dbg.wfc_avail_ovr=1
 
+# Incremental FS
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.incremental.enable=1
+
 # Keymaster 4.0 - TDES support
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.keystore_desede=true
