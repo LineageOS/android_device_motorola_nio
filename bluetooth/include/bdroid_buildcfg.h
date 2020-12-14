ca6e6ad31bd4274cb52d4a10a365bd8215a4e455
Erfan Abdi <erfangplus@gmail.com>
2020-12-14 04:28:20 +0330

    sm7250-common: Commonize bluetooth header

---
+++ bluetooth/include/bdroid_buildcfg.h
@@ -0,0 +1,58 @@
+/*
+ *
+ *  Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ *  Not a Contribution, Apache license notifications and license are retained
+ *  for attribution purposes only.
+ *
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _BDROID_BUILDCFG_H
+#define _BDROID_BUILDCFG_H
+
+#pragma push_macro("PROPERTY_VALUE_MAX")
+
+#include <cutils/properties.h>
+#include <string.h>
+
+static inline const char* BtmGetDefaultName()
+{
+    char product_device[PROPERTY_VALUE_MAX];
+    property_get("ro.product.device", product_device, "");
+
+    if (strstr(product_device, "nairo"))
+        return "Moto G 5G Plus";
+    if (strstr(product_device, "racer"))
+        return "Motorola Edge";
+    if (strstr(product_device, "smith"))
+        return "Motorola Razr 2";
+
+    // Fallback to Moto Generic
+    return "Motorola";
+}
+
+#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
+// Disables read remote device feature
+#define BTM_WBS_INCLUDED TRUE
+#define BTIF_HF_WBS_PREFERRED TRUE
+
+#define BLE_VND_INCLUDED   TRUE
+// skips conn update at conn completion
+#define BT_CLEAN_TURN_ON_DISABLED 1
+
+#pragma pop_macro("PROPERTY_VALUE_MAX")
+
+#endif
+
