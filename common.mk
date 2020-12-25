c2c9a46967100da9150f70e2c1d8283e469093ce
Erfan Abdi <erfangplus@gmail.com>
2020-12-25 13:40:39 +0330

    sm7250-common: Enable QTI bluetooth stack

--- common.mk
+++ common.mk
@@ -133,6 +133,7 @@ PRODUCT_PACKAGES += \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
+    libsndmonitor \
     libvolumelistener \
     tinymix \
     libtinycompress \
@@ -163,9 +164,10 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    BluetoothQti \
     liba2dpoffload \
-    libbthost_if \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
