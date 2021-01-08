df6ad1f1182b6f6c465337dd43399fb4e7efc048
Erfan Abdi <erfangplus@gmail.com>
2021-01-08 05:02:05 +0330

    sm7250-common: Import bootctrl from nash
    * Good old moto, slot A and B partitions are on different disks
    * We need: https://github.com/LineageOS/android_device_motorola_nash/commit/ba70a48f8d0ab12666d2d37e31f581697423ae02

--- common.mk
+++ common.mk
@@ -159,8 +159,8 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl:64 \
     android.hardware.boot@1.0-service \
     android.hardware.boot@1.0-impl.recovery \
-    bootctrl.lito \
-    bootctrl.lito.recovery
+    bootctrl.qcom \
+    bootctrl.qcom.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
