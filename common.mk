b0f870a789c5fc7696a26e7e64e6fa07a7886bf6
Andrew Hexen <SyberHexen@gmail.com>
2021-02-15 06:33:18 -0800

    sm7250-common: switch to sensors@2.0 multihal
    Change-Id: Id5104e2376d9b32f28946fbfd7acdbb35eb5cea6

--- common.mk
+++ common.mk
@@ -444,8 +444,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
+    android.hardware.sensors@2.0-service.multihal \
     libsensorndkbridge
 
 # Servicetracker
