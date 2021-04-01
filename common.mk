9710a09b9ccda0dc8f21969c7f80677ab0143ceb
electimon <electimon@gmail.com>
2021-04-01 05:39:15 +0000

    sm8250-common: Switch to QTI vibrator AIDL
    * We like aidls here
    
    Signed-off-by: electimon <electimon@gmail.com>

--- common.mk
+++ common.mk
@@ -458,8 +458,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    vendor.qti.hardware.vibrator.service
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
