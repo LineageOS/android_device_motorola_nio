90a1feeccae7af612a52835156d8222ecd49d9b1
electimon <electimon@gmail.com>
2021-04-02 02:07:09 +0000

    Revert "sm8250-common: Import libperfmgr"
    This reverts commit 3cc5dd1c3ce0f9819bbabb37200adda759a76b6c.

--- common.mk
+++ common.mk
@@ -427,10 +427,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.sm8250-libperfmgr
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/power-libperfmgr/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    android.hardware.power-service-qti
 
 # QCOM
 PRODUCT_COPY_FILES += \
@@ -482,9 +479,7 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH) \
-    hardware/google/interfaces \
-    hardware/google/pixel
+    $(LOCAL_PATH)
 
 # Telephony
 PRODUCT_PACKAGES += \
