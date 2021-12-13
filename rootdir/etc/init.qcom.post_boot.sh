9ab9cc62c23f77ce325c3dbc7eaaf96370721fb7
LADI RAM SAI <quic_lramsai@quicinc.com>
2021-12-13 11:48:34 +0530

    Changes in cpuset affinity parameters for background tasks on gvm
    Change-Id: Ieff067f713d5535d764b8c5640594df02ee5db32
    Signed-off-by: LADI RAM SAI <quic_lramsai@quicinc.com>

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -5378,9 +5378,9 @@ case "$target" in
 				echo 0 > $npubw/bw_hwmon/idle_mbps
 		                echo 40 > $npubw/polling_interval
 				echo 0 > /sys/devices/virtual/npu/msm_npu/pwr
-	    done
-	done
-
+	                      done
+	           done
+	fi
 	# memlat specific settings are moved to seperate file under
 	# device/target specific folder
 	setprop vendor.dcvs.prop 1
@@ -5427,7 +5427,6 @@ case "$target" in
 			configure_automotive_sku_parameters
 		   fi
 		fi
-	fi
     ;;
 esac
 
