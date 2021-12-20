25d14e2da15467c0f419eac760558cf148a4f2ca
dianlujitao <dianlujitao@lineageos.org>
2021-12-20 14:59:14 +0800

    Revert "sm8250-common: init: add init overrides for safetynet"
    This reverts commit 16bf292e44728cf6ac3ac76791da060ea747a8e0.
    
    Change-Id: I543989b6dac57c80e1b273fc295d3fb379ef43da

--- init/init_kona.cpp
+++ init/init_kona.cpp
@@ -37,7 +37,6 @@
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
-#include <sys/stat.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
@@ -51,55 +50,6 @@ char const *heapsize;
 char const *heapminfree;
 char const *heapmaxfree;
 char const *heaptargetutilization;
-
-/* From Magisk@jni/magiskhide/hide_utils.c */
-static const char *snet_prop_key[] = {
-    "ro.boot.vbmeta.device_state",
-    "ro.boot.flash.locked",
-    "ro.boot.selinux",
-    "ro.boot.veritymode",
-    "ro.boot.warranty_bit",
-    "ro.warranty_bit",
-    "ro.build.type",
-    "ro.build.tags",
-    "ro.build.selinux",
-    "ro.debuggable",
-    NULL
-};
-
- static const char *snet_prop_value[] = {
-    "locked",
-    "1",
-    "enforcing",
-    "enforcing",
-    "0",
-    "0",
-    "user",
-    "release-keys",
-    "1",
-    "0",
-    NULL
-};
-
-void property_override(char const prop[], char const value[], bool add = true)
-{
-    prop_info *pi;
-
-    pi = (prop_info *) __system_property_find(prop);
-    if (pi)
-        __system_property_update(pi, value, strlen(value));
-    else if (add)
-        __system_property_add(prop, strlen(prop), value, strlen(value));
-}
-
-static void workaround_snet_properties() {
-
-    // Hide all sensitive props
-    for (int i = 0; snet_prop_key[i]; ++i) {
-        property_override(snet_prop_key[i], snet_prop_value[i]);
-    }
-
-}
 
 void check_device()
 {
@@ -144,8 +94,4 @@ void vendor_load_properties()
     SetProperty("dalvik.vm.heaptargetutilization", heaptargetutilization);
     SetProperty("dalvik.vm.heapminfree", heapminfree);
     SetProperty("dalvik.vm.heapmaxfree", heapmaxfree);
-
-    // SafetyNet workaround
-    property_override("ro.boot.verifiedbootstate", "green");
-    workaround_snet_properties();
 }
