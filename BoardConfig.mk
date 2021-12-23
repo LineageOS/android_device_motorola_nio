451f4eb6edd819c55541d26c9ca097d5fe584913
dianlujitao <dianlujitao@lineageos.org>
2021-12-23 21:19:24 +0800

    nio: Add IFAA service
    Change-Id: I1cd1fff8a9c9f116e68a687269bd48e7c03b77b6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -243,6 +243,7 @@ VENDOR_SECURITY_PATCH := 2021-08-01
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Android Verified Boot
