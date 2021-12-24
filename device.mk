646e4ad3618201b50a2f8e7ad75a6db5ec5fa295
dianlujitao <dianlujitao@lineageos.org>
2021-12-24 15:42:45 +0800

    nio: Clean up audio and a2dp blobs
    Change-Id: Ic0e262d3226dfe8ffbcd01b8c483a2ae877b0710

--- device.mk
+++ device.mk
@@ -142,6 +142,9 @@ PRODUCT_PACKAGES += \
     libtinycompress \
     libtinycompress.vendor
 
+PRODUCT_PACKAGES += \
+    libqcomvoiceprocessing
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_ext_spkr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_ext_spkr.conf \
