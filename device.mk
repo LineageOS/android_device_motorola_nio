b07014764ae48373e29a94556ae27f67b39712ec
dianlujitao <dianlujitao@lineageos.org>
2021-12-26 23:13:43 +0800

    nio: Add public.libraries-qti.txt to system_ext
    Change-Id: I236b10741f014c7ab75b4a0e7c5af264563361fe

--- device.mk
+++ device.mk
@@ -25,7 +25,8 @@ $(call inherit-product-if-exists, vendor/motorola/nio/nio-vendor.mk)
 
 # Additional native libraries
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt \
+    $(LOCAL_PATH)/configs/public.libraries.system_ext-qti.txt:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/public.libraries-qti.txt
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
