284563a1a2f122ccd55375f649450997fe026083
dianlujitao <dianlujitao@lineageos.org>
2021-12-26 22:53:17 +0800

    nio: Remove duplicated msm_irqbalance.conf from device
    Change-Id: I57f69c701c33b5d36e3531c2e0b5072fde5a4357

--- device.mk
+++ device.mk
@@ -343,10 +343,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
-# IRQ
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
-
 # IPv6
 PRODUCT_PACKAGES += \
     ethertypes \
