27bcbb034a3f669fc08a9ecd2c063c0c08d35c5a
dianlujitao <dianlujitao@lineageos.org>
2021-12-26 23:02:34 +0800

    nio: Drop hotwordenrollment permissions
     * We don't even have them on stock, do we?
    
    Change-Id: I609befea6a8512877492299e7ef56a467e546f4c

--- device.mk
+++ device.mk
@@ -328,10 +328,6 @@ PRODUCT_PACKAGES += \
     libhidltransport \
     libhwbinder
 
-# HotwordEnrollement app permissions
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
-
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
