d9acb27bc564d43127174f0d60f5379b8747c47b
dianlujitao <dianlujitao@lineageos.org>
2021-12-26 23:28:12 +0800

    nio: Correct privapp-permissions-qti-system-ext.xml
     * One is missing and the other is redundant. They just happen to be the
       same name.
    
    Change-Id: I49816932f947140a5447f81c123e6f5b4bb9bb21

--- device.mk
+++ device.mk
@@ -435,7 +435,6 @@ PRODUCT_PACKAGES += \
 
 # QCOM
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/configs/qti_whitelist_system_ext.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/qti_whitelist_system_ext.xml
 
