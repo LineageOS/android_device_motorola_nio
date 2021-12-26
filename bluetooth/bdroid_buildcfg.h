7020d94c4c3e644119f2e90ba8cfe1df141ce1a2
dianlujitao <dianlujitao@lineageos.org>
2021-12-26 22:23:52 +0800

    nio: Update bdroid_buildcfg.h from QSSI
    Change-Id: I1da1e0c9734e9bba815061af124d544b7106e69e

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,8 +1,6 @@
 /*
- *
  *  Copyright (c) 2013, The Linux Foundation. All rights reserved.
- *  Not a Contribution, Apache license notifications and license are retained
- *  for attribution purposes only.
+ *  Not a Contribution.
  *
  * Copyright (C) 2012 The Android Open Source Project
  *
@@ -21,35 +19,14 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-
-#include <stdint.h>
-#include <string.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-int property_get(const char *key, char *value, const char *default_value);
-#ifdef __cplusplus
-}
-#endif
-
-static inline const char* BtmGetDefaultName()
-{
-    char product_device[92];
-    property_get("ro.product.device", product_device, "");
-
-    if (strstr(product_device, "nio"))
-        return "Motorola Edge S";
-
-    // Fallback to Moto Generic
-    return "Motorola";
-}
-
-#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
+//#define BTM_DEF_LOCAL_NAME   "QCOM-BTD"
 // Disables read remote device feature
-#define BTM_WBS_INCLUDED TRUE
-#define BTIF_HF_WBS_PREFERRED TRUE
-
+#define MAX_ACL_CONNECTIONS   16
+#define MAX_L2CAP_CHANNELS    32
 #define BLE_VND_INCLUDED   TRUE
-#endif
+#define GATT_MAX_PHY_CHANNEL  10
+// skips conn update at conn completion
+#define BT_CLEAN_TURN_ON_DISABLED 1
 
+#define AVDT_NUM_SEPS 35
+#endif
