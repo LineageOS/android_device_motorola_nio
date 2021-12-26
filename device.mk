043418459c47e4c58bcaf1a01c911c7e23f07bca
dianlujitao <dianlujitao@lineageos.org>
2021-12-27 00:55:29 +0800

    nio: Don't build livedisplay sysfs hal
    Change-Id: Idd94a102980eae251a6462294071ff5c2c14404a

--- device.mk
+++ device.mk
@@ -343,8 +343,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.0-service-sysfs
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_PACKAGES += \
