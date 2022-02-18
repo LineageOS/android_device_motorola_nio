8aa43cd57db9a210da46dda24ce67c4d020096bf
dianlujitao <dianlujitao@lineageos.org>
2022-02-18 23:06:04 +0800

    nio: Fix invalid usage of log utility
    Fix derp in c756ad8dbbf97a9775228d160c4481af994e0d72
    
    Change-Id: I7a4d5291a11d823f0cef024b136cfc703fa64fc0

--- rootdir/etc/init.oem.fingerprint2.sh
+++ rootdir/etc/init.oem.fingerprint2.sh
@@ -11,7 +11,7 @@
 script_name=${0##*/}
 script_name=${script_name%.*}
 function notice {
-    log -t "$scriptname" -i "$*"
+    log -t "$scriptname" -p i "$*"
 }
 
 persist_fps_id=/mnt/vendor/persist/fps/vendor_id
