e7414238a3442b8e02a15175da3fcf680d4ee0cc
dianlujitao <dianlujitao@lineageos.org>
2022-02-26 22:58:52 +0800

    nio: Set device model to XT2125-4
    Change-Id: Ia24df410be30f8a9a36e32c280874a1d897d6fc0

--- lineage_nio.mk
+++ lineage_nio.mk
@@ -28,7 +28,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_NAME := lineage_nio
 PRODUCT_DEVICE := nio
 PRODUCT_BRAND := motorola
-PRODUCT_MODEL := Motorola Edge S
+PRODUCT_MODEL := XT2125-4
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_SHIPPING_API_LEVEL := 30
 
