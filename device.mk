6a69361b6ac66a23968354118086bf798271dce8
dianlujitao <dianlujitao@lineageos.org>
2022-03-16 17:05:56 +0800

    nio: Drop Snap
    Change-Id: Ie5e2c3d337a3d80795d6d307eb5cc4326d73bbd0

--- device.mk
+++ device.mk
@@ -210,7 +210,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libgui_vendor \
-    Snap \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 # Common init scripts
