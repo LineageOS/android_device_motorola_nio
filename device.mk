0090f314d0bd980e3974fc0c7daa8740f2c5d134
LuK1337 <priv.luk@gmail.com>
2022-03-16 01:30:14 +0100

    nio: Remove trust HAL from product packages
    USB 1.3 HAL now provides the same functionality and even more.
    
    Change-Id: Idda908a90d3b935e1065eb41abec4babeefc4fd0

--- device.mk
+++ device.mk
@@ -498,10 +498,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
