b825240f8cec7543051d15204abeea0eaf73f257
Sal Savage <salsavage@google.com>
2022-04-15 15:38:17 -0700

    nio: Replace BTM_DEF_LOCAL_NAME with a sysprop
    Change-Id: I2c90efac01e61500eb1be23bb0ae6f96c2acdc53

--- device.mk
+++ device.mk
@@ -21,6 +21,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+    bluetooth.device.default_name=moto g100
+
 # Init
 PRODUCT_PACKAGES += \
     init.device.rc \
