89826edcc906a13f1a559987b3df3938ffe4be0b
dianlujitao <dianlujitao@lineageos.org>
2022-09-03 14:40:46 +0800

    nio: Move audio configurations from common
    Change-Id: I280f79f42a7ca77fad6409832958681ea9faead1

--- device.mk
+++ device.mk
@@ -16,5 +16,10 @@ PRODUCT_PACKAGES += \
     FrameworksResNio \
     SystemUIResNio
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/motorola/nio/nio-vendor.mk)
