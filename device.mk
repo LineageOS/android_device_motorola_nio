c46a7b066d572d84b2f2ec335f95046ca932e479
LuK1337 <priv.luk@gmail.com>
2023-10-19 23:08:10 +0200

    nio: Build AIDL NXP NFC HAL
    Change-Id: I6ace3ac149c84d2cb9e2773e7ef5ddbf24c35b92
    (cherry picked from commit 4e42a8258972b9904ca2b515e09252759b651fac)

--- device.mk
+++ device.mk
@@ -37,7 +37,7 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2-service
+    android.hardware.nfc-service.nxp
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
