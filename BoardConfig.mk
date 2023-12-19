0198f7f2c0ed8f06ed4d872fec7a30abad6f191a
dianlujitao <dianlujitao@lineageos.org>
2023-12-19 20:42:17 +0800

    nio: Use MMI touchscreen class to toggle st2w/dt2w
    Change-Id: I92518880d4d8f5e4567ccdad8604fde6d480800c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,11 +25,5 @@ BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD)
 # SEPolicy
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
-# Touch
-SOONG_CONFIG_NAMESPACES += MOTO_KONA_TOUCH
-SOONG_CONFIG_MOTO_KONA_TOUCH := SINGLE_TAP_PATH
-SOONG_CONFIG_MOTO_KONA_TOUCH_SINGLE_TAP_PATH := /sys/class/touchscreen/NVT-ts/single_click
-TARGET_TAP_TO_WAKE_NODE := /sys/class/touchscreen/NVT-ts/double_click
-
 # Inherit from the proprietary version
 -include vendor/motorola/nio/BoardConfigVendor.mk
