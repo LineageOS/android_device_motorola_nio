6f2479dd024092d2894713540a5472e5f2b9cfb4
dianlujitao <dianlujitao@lineageos.org>
2024-01-07 14:42:54 +0800

    nio: Update blobs from S1RN32.55-16-13
    Change-Id: I80eec032a9d2fefc3fc298d33684346c9e61e0c5

--- lineage_nio.mk
+++ lineage_nio.mk
@@ -24,7 +24,7 @@ PRODUCT_MANUFACTURER := motorola
 PRODUCT_SYSTEM_NAME := nio_retcn
 
 # Build info
-BUILD_FINGERPRINT := "motorola/nio_retcn/nio:12/S1RN32.55-16-2/125d6-32b4dd:user/release-keys"
+BUILD_FINGERPRINT := "motorola/nio_retcn/nio:12/S1RN32.55-16-13/fce21a-b9f4e9:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="nio_retcn-user 12 S1RN32.55-16-2 125d6-32b4dd release-keys" \
+    PRIVATE_BUILD_DESC="nio_retcn-user 12 S1RN32.55-16-13 fce21a-b9f4e9 release-keys" \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
