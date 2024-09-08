99e1921efec8606dcd2a886c1928223ff97a2f3a
dianlujitao <dianlujitao@lineageos.org>
2024-09-08 23:11:53 +0800

    nio: move product inheritance to common
    Change-Id: I3e3671d47c268d4cbb82ad44fb737ccccbb41455

--- lineage_nio.mk
+++ lineage_nio.mk
@@ -4,22 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Inherit from nio device
 $(call inherit-product, device/motorola/nio/device.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_nio
 PRODUCT_DEVICE := nio
-PRODUCT_BRAND := motorola
 PRODUCT_MODEL := XT2125-4
-PRODUCT_MANUFACTURER := motorola
 
 PRODUCT_SYSTEM_NAME := nio_retcn
 
