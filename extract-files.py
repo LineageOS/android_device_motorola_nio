33c7ea277ef64c2ee7c12830a3ec095579a03b29
LuK1337 <priv.luk@gmail.com>
2024-10-15 22:11:44 +0200

    nio: Switch to py extract utils
    Change-Id: I5618bf10c107df579a540e2393b222128961e670

---
+++ extract-files.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixup_vendorcompat,
+    lib_fixups_user_type,
+    libs_proto_3_9_1,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+namespace_imports = [
+    'vendor/motorola/sm8250-common',
+    'vendor/qcom/opensource/display',
+]
+
+lib_fixups: lib_fixups_user_type = {
+    libs_proto_3_9_1: lib_fixup_vendorcompat,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    ('vendor/lib/libmot_chi_desktop_helper.so', 'vendor/lib64/libmot_chi_desktop_helper.so'): blob_fixup()
+        .add_needed('libgui_shim_vendor.so'),
+    ('vendor/lib64/com.qti.feature2.gs.so', 'vendor/lib64/com.qti.feature2.rt.so',
+     'vendor/lib64/hw/camera.qcom.so', 'vendor/lib64/hw/com.qti.chi.override.so'): blob_fixup()
+        .binary_regex_replace(b'camera.mot.is.coming.cts', b'vendor.camera.coming.cts'),
+    'vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so': blob_fixup()
+        .sig_replace('13 0A 00 94', '1F 20 03 D5'),
+}
+module = ExtractUtilsModule(
+    'nio',
+    'motorola',
+    namespace_imports=namespace_imports,
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    check_elf=True,
+)
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'sm8250-common', module.vendor
+    )
+    utils.run()
