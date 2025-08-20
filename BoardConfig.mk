3359e23f5c8990f027577eaa855e7ea4ac2c0e10
LuK1337 <priv.luk@gmail.com>
2025-08-20 19:20:35 -0400

    nio: Migrate to AIDL LiveDisplay HAL
    Change-Id: I4e8be836e94d442b40c63a1ac7dade3c9d9c784c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,9 +14,6 @@ TARGET_BOOTLOADER_BOARD_NAME := nio
 # Display
 TARGET_SCREEN_DENSITY := 480
 
-# HIDL
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
-
 # Kernel
 TARGET_KERNEL_CONFIG += vendor/ext_config/nio-default.config
 BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += nova_0flash_mmi.ko
