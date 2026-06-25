269cefb2c8f5fb91fab5a873feb88e144bfb1025
Nolen Johnson <johnsonnolen@gmail.com>
2026-06-25 18:52:20 -0400

    nio: Use libgui_buffer_shim for necessary libs
    * libmot_chi_desktop_helper.so wants pre-refactor 3-arg
      `acquireBuffer(BufferItem*, nsecs_t, bool)` and 2-arg
      `releaseBuffer(const BufferItem&, const sp<Fence>&)`.
    * Sadly this breaks 32-bit build, which /some/ people might
      be using, so add a dedicated target for this that devices
      can link.
    
    Change-Id: I1d61678ae7a5b05ddbbb58281e875a575d94d1cb

--- extract-files.py
+++ extract-files.py
@@ -28,8 +28,8 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
-    ('vendor/lib/libmot_chi_desktop_helper.so', 'vendor/lib64/libmot_chi_desktop_helper.so'): blob_fixup()
-        .add_needed('libgui_shim_vendor.so'),
+    'vendor/lib64/libmot_chi_desktop_helper.so': blob_fixup()
+        .add_needed('libgui_buffer_shim_vendor.so'),
     ('vendor/lib64/com.qti.feature2.gs.so', 'vendor/lib64/com.qti.feature2.rt.so',
      'vendor/lib64/hw/camera.qcom.so', 'vendor/lib64/hw/com.qti.chi.override.so'): blob_fixup()
         .binary_regex_replace(b'camera.mot.is.coming.cts', b'vendor.camera.coming.cts'),
